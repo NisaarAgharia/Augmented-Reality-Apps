cb8d698f5a93727f3d99c437935694be70600dd6
Nisaar <nagharia786@gmail.com>
2021-09-09 12:26:20 +0530

    ARPlane changer cs file

---
+++ arfoundation-master/Assets/ARPlaneChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPlaneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material[] materials;
+    private int pos =0;
+
+    public MeshRenderer currentGroundPlane;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+//Function for changing planes based on numbers and increments
+    public void NextPlane(){
+        pos=pos+1;
+        if(pos>=materials.Length)
+        {
+            pos=0;
+        }
+      setPostTo(pos);
+    }
+
+    public void setPostTo(int newPosition){
+        pos= newPosition;
+
+          MeshRenderer[] renderers= transform.GetComponentsInChildren<MeshRenderer>();
+        for(int i=0;i<renderers.Length;i++){
+            renderers[i].material=materials[pos];
+        }
+        currentGroundPlane.material=materials[pos];
+
+    }
+}
