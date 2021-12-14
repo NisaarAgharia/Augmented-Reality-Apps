31ccec9ba7ebca5f02187c371a2978465d1cc272
Nisaar <nagharia786@gmail.com>
2021-12-14 19:16:31 +0530

    Graphical files

---
+++ arfoundation-master/Assets/Marcos Schultz/MS Sposts Car/Others/PromotionalMSVS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromotionalMSVS : MonoBehaviour {
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (20, 20, 400, 50), "Need a vehicle controller? CLICK HERE")) {
+			Application.OpenURL ("https://assetstore.unity.com/packages/tools/physics/ms-vehicle-system-vehicle-controller-88035");
+		}
+	}
+}
---
+++ arfoundation-master/Assets/Marcos Schultz/MS Sposts Car/Others/WheelMeshes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class WheelInstance{
+	public WheelCollider _wheelCollider;
+	public GameObject _wheelMesh;
+}
+
+public class WheelMeshes : MonoBehaviour {
+
+	public WheelInstance[] _vehicleWheels;
+
+	void Update () {
+		for (int x = 0; x < _vehicleWheels.Length; x++) {
+			if (_vehicleWheels [x]._wheelCollider && _vehicleWheels [x]._wheelMesh) {
+				Vector3 tempPos;
+				Quaternion tempRot;
+				_vehicleWheels [x]._wheelCollider.GetWorldPose (out tempPos, out tempRot);
+				_vehicleWheels [x]._wheelMesh.transform.position = tempPos;
+				_vehicleWheels [x]._wheelMesh.transform.rotation = tempRot;
+			}
+		}
+	}
+}
